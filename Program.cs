420ab0fe48b948f7f7346b98c25c879d046eb7fd
AreiaAruanda <144889202+AreiaAruanda@users.noreply.github.com>
2024-01-31 14:10:58 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.Metrics;
+using System.Numerics;
+
+namespace Rövarsprak
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("What would you like to translate into rövarspråk?\n");
+            Console.WriteLine("Please enter a word or sentence: ");
+            string word = Console.ReadLine();
+            string wordNew = word.ToLower();
+
+            for (int i = 0; i < wordNew.Length; i++)
+            {
+                if (!"aeiouäåö ,;:!?.".Contains(wordNew[i]))
+                    Console.Write($"{wordNew[i]}o{wordNew[i]}");
+                else
+                    Console.Write(wordNew[i]);
+            };
+            Console.WriteLine();
+        }
+    }
+}
